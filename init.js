8a1286ed32f04d8b62f59e9f68417e8bda4329bf
Rodrigo D <neonrok@pm.me>
2025-01-03 16:51:34 +0000

    problemas

--- init.js
+++ init.js
@@ -7,29 +7,43 @@ const scene = new THREE.Scene();
 // create a camera, which defines where we're looking at
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.y = 2;
-camera.position.z = 5; // move the camera from the world center (2,2,5)
-camera.lookAt(scene.position); //point the camera to the center of the scene
+camera.position.z = 5;
+camera.lookAt(scene.position); 
 
 // create a render and set the size
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-// configure renderer clear color
 renderer.setClearColor("#000000");
-// add the output of the renderer to an HTML element (this case, the body)
 document.body.appendChild(renderer.domElement);
 
+//controls
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', render);
 
+//construção personagem
+let drag = {
+    x: 0, y: -1, z:0, ry: 0, angle: 0, body: null, head: null, arms: [],
+    material: new THREE.MeshLambertMaterial({ color: 0xffffff }), 
 
-// start the animation
-renderer.setAnimationLoop(render);
+    init(){
+        //corpo
+        const bodyGeometry = new THREE.BoxGeometry(1, 1, 1)
+        this.body = new THREE.Mesh(bodyGeometry, this.material)
+        scene.add(this.body)
 
+        //cabeça
+        const headGeometry = new THREE.BoxGeometry(1.2, 1.2, 1.2)
+        this.head = new THREE.Mesh(headGeometry, this.material)
+        this.body.add(this.head)
+        this.head.position.y = 1.1;
+    },
+}
 
+drag.init()
+drag.body.rotation.y = THREE.MathUtils.degToRad(-15)
 
+//função render
 function render() {
-    // TODO
-
-    // render the scene ("draw" the scene into the Canvas, using the camera's point of view)
     renderer.render(scene, camera);
 };
 
@@ -43,4 +57,5 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     // Adjust the pixel ratio of the renderer based on the device's pixel density
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    render()
 })
