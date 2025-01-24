9d978407327ef855b044e90d81dc4d5fc3d2cf8b
Rodrigo D <neonrok@pm.me>
2025-01-24 16:47:18 +0000

    asas animation finich

--- init.js
+++ init.js
@@ -33,6 +33,7 @@ scene.add( axesHelper );
 let drag = {
     x: 0, y: -1, z:0, ry: 0, angle: 0, body: null, pescoço:null, head:null,
     boca: null, head_x: [], bodyP2: null, menbers: [], asas: new THREE.Object3D(),
+    sub_asas1: new THREE.Object3D(), sub_asas2: new THREE.Object3D(),
     material: new THREE.MeshLambertMaterial({ color: 0x4b4b4b, wireframe: false, side:THREE.DoubleSide}), 
     materialb: new THREE.MeshLambertMaterial({ color: 0xaa00aa, wireframe: false, side:THREE.DoubleSide}),
 
@@ -242,11 +243,10 @@ let drag = {
         };
         //Asas++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++
         bodyP2.add(this.asas);
+        
         for(let i=0; i<2; i++){
             let place_loc = {x: xbody2/2, y: ybody2*0.4, z: zbody2/2};
             let gen_As_axesHelper = new THREE.AxesHelper( 1 );
-            let sub_Menb_axesHelper = new THREE.AxesHelper( 1 );
-            let sub_Art_axesHelper = new THREE.AxesHelper( 1 );
             let x_y;
             switch(i){
                 case 0: x_y = 1;break;
@@ -266,11 +266,12 @@ let drag = {
             );
             AsAng.add(arm_1)
             arm_1.position.y=a1g.y/2
-            console.log( this.asas)
-            let subAng = new THREE.Object3D();
-            subAng.add(sub_Menb_axesHelper);
-            arm_1.add(subAng);
-            subAng.position.y= a1g.y/2.1;
+
+            let angleB = new THREE.Object3D()
+
+            arm_1.add(angleB);
+            angleB.add(this.sub_asas1);
+            angleB.position.y= a1g.y/2.1;
 
             let geometry1 = new THREE.BufferGeometry();
             const points = [] // define array of vertices
@@ -285,45 +286,72 @@ let drag = {
             As_C1.position.z =-0.4
             As_C1.rotation.y= 0.2
             As_C1.rotation.z= -0.1
-
-            //2ªparte das asas +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ ++ ++
             let a2g = {x:0.3, y:0.3, z:1.8};
             let As_2 = new THREE.Mesh(
                 new THREE.BoxGeometry(a2g.x, a2g.y, a2g.z),
                 this.material,
             );
-            subAng.add(As_2)
+            angleB.add(As_2)
             As_2.position.z=-a2g.z/2 
-
-            let subAngArt = new THREE.Object3D();
-            subAngArt.add(sub_Art_axesHelper);
-            subAng.add(subAngArt);
-            subAngArt.rotation.x = -0.2
-            let a3g = {x:0.3, y:2.5, z:0.3};
-            let arm_3 = new THREE.Mesh(
-                new THREE.BoxGeometry(a3g.x, a3g.y, a3g.z),
-                this.material,
-            );
-            subAngArt.add(arm_3)
-
-            let geometry2 = new THREE.BufferGeometry();
-            const points2 = [] // define array of vertices
-            points2.push(new THREE.Vector3(0.3, -2.2, 0.3))
-            points2.push(new THREE.Vector3(0.3, 0.5, -1.5))
-            points2.push(new THREE.Vector3(0, 0.5, 0.3))
-            geometry2.setFromPoints(points2);
-            
-            let As_C2 = new THREE.Mesh(geometry2, this.materialb);
-            arm_3.add(As_C2)
-            As_C2.position.y =0.7
-            As_C2.position.z =-0.4
-            As_C2.rotation.y= 0.2
-            As_C2.rotation.z= -0.1
-
-            arm_3.position.y=a3g.y/2 
-            arm_3.position.z=a3g.z/2 -0.2
-          
         }
+        const AH = new THREE.AxesHelper( 1 );
+        this.asas.children[0].children[1].children[0].add(this.sub_asas1)
+        this.sub_asas1.add(AH)
+        let subAng = new THREE.Object3D();
+        this.sub_asas1.add(subAng);
+        subAng.rotation.x = -0.2
+        let a3g = {x:0.3, y:2.5, z:0.3};
+        let arm_3 = new THREE.Mesh(
+            new THREE.BoxGeometry(a3g.x, a3g.y, a3g.z),
+            this.material,
+        );
+        subAng.add(arm_3)
+
+        let geometry2 = new THREE.BufferGeometry();
+        const points2 = [] // define array of vertices
+        points2.push(new THREE.Vector3(0.3, -2.2, 0.3))
+        points2.push(new THREE.Vector3(0.3, 0.5, -1.5))
+        points2.push(new THREE.Vector3(0, 0.5, 0.3))
+        geometry2.setFromPoints(points2);
+            
+        let As_C2 = new THREE.Mesh(geometry2, this.materialb);
+        arm_3.add(As_C2)
+        As_C2.position.y =0.7
+        As_C2.position.z =-0.4
+        As_C2.rotation.y= 0.2
+        As_C2.rotation.z= -0.1
+
+        arm_3.position.y=a3g.y/2 
+        arm_3.position.z=a3g.z/2 -0.2
+
+        const AH2 = new THREE.AxesHelper( 1 );
+        this.asas.children[1].children[1].children[0].add(this.sub_asas2)
+        this.sub_asas2.add(AH2)
+        let subAng1 = new THREE.Object3D();
+        this.sub_asas2.add(subAng1);
+        subAng1.rotation.x = -0.2
+        let arm_32 = new THREE.Mesh(
+            new THREE.BoxGeometry(a3g.x, a3g.y, a3g.z),
+            this.material,
+        );
+        subAng1.add(arm_32)
+
+        let geometry22 = new THREE.BufferGeometry();
+        const points22 = [] // define array of vertices
+        points22.push(new THREE.Vector3(0.3, -2.2, 0.3))
+        points22.push(new THREE.Vector3(0.3, 0.5, -1.5))
+        points22.push(new THREE.Vector3(0, 0.5, 0.3))
+        geometry22.setFromPoints(points2);
+            
+        let As_C22 = new THREE.Mesh(geometry22, this.materialb);
+        arm_32.add(As_C22)
+        As_C22.position.y =0.7
+        As_C22.position.z =-0.4
+        As_C22.rotation.y= 0.2
+        As_C22.rotation.z= -0.1
+
+        arm_32.position.y=a3g.y/2 
+        arm_32.position.z=a3g.z/2 -0.2
 
         //Cauda +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++
         let zbody3 = 1.7
@@ -451,7 +479,7 @@ let pointLightHelper = new THREE.PointLightHelper(light2, 0.4);
 pointLightHelper.name = "helper";
 scene.add(pointLightHelper);
 
-
+console.log(drag.sub_asas1)
 renderer.setAnimationLoop(render)
 //função render - responsabel pela animação EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 function render() {
@@ -461,8 +489,9 @@ function render() {
     if (drag.asas.children[0].rotation.z< -2){exbox = 1};
     drag.asas.children[0].rotation.z += spd * exbox 
     drag.asas.children[1].rotation.z += -spd * exbox 
-    //console.log(drag.asas)
-    console.log(drag.asas.children[0].rotation.z)
+    drag.sub_asas1.children[1].rotation.z += (spd) * exbox 
+    drag.sub_asas2.children[1].rotation.z += -spd * exbox
+    
     renderer.render(scene, camera);
 };
 
